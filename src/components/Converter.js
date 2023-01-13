4754df1e797fa4afecdcb07fbff9f285448fbc49
Anjum <anjumshaikh152535@gmail.com>
2023-01-13 11:31:52 +0530

    validation

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //user can convert rates of different coins
 function Converter() {
-  const [inputValue, setinputValue] = useState("");
+  const [inputValue, setinputValue] = useState();
   const [inputValueTwo, setinputValueTwo] = useState(1);
   const [value1, setvalue1] = useState(1);
   const [value2, setvalue2] = useState(1);
@@ -22,74 +22,89 @@ function Converter() {
       });
   }, []);
 
-  const convert = () => {
-    let result = ((value2 / value1) * inputValue).toFixed(2);
-    setinputValueTwo(result);
+  const convert = (e) => {
+    e.preventDefault();
+    if (!inputValue) {
+      setinputValueTwo("NaN");
+    } else {
+      let result = ((value2 / value1) * inputValue).toFixed(2);
+      setinputValueTwo(result);
+    }
   };
 
   return (
-    <div className="pl-4 mt-6 pb-2">
-      <div className="font-bold pl-16 mb-10">
-        <h1>Exchange Coins</h1>
-      </div>
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <td className="text-green-600 lg:pr-4">From</td>
-              <td>
-                <input
-                  className="h-8 lg:w-40 md:w-20 rounded mb-2 bg-purple-100 text-center"
-                  value={inputValue}
-                  type="number"
-                  name="currency-amount-1"
-                  onChange={(e) => setinputValue(e.target.value)}
-                />
-              </td>
-              <td>
-                <select
-                  className="w-20  p-1 mb-2 rounded bg-purple-100"
-                  onChange={(e) => setvalue1(e.target.value)}
-                >
-                  {coin &&
-                    Object.values(coin).map((d, k) => (
-                      <option key={k} value={d.value}>
-                        {d.name}
-                      </option>
-                    ))}
-                </select>
-              </td>
-            </tr>
+    <div className="pl-4 mt-6 pb-2 ">
+      <form>
+        <div className="font-bold pl-16 mb-10">
+          <h1>Exchange Coins</h1>
+        </div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td className="text-green-600 lg:pr-4">From</td>
+                <td>
+                  <select
+                    className="w-20  p-1 mb-2 rounded bg-purple-100 lg:mr-4 sm:mr-4 "
+                    onChange={(e) => setvalue1(e.target.value)}
+                  >
+                    {coin &&
+                      Object.values(coin).map((d, k) => (
+                        <option key={k} value={d.value}>
+                          {d.name}
+                        </option>
+                      ))}
+                  </select>
+                </td>
 
-            <tr>
-              <td className="text-red-600">To</td>
-              <td>
-                <p className=" bg-purple-100 text-center">{inputValueTwo}</p>
-              </td>
-              <td>
-                <select
-                  className="w-20  p-1 rounded  bg-purple-100"
-                  onChange={(e) => setvalue2(e.target.value)}
-                >
-                  {coin &&
-                    Object.values(coin).map((d, k) => (
-                      <option key={k} value={d.value}>
-                        {d.name}
-                      </option>
-                    ))}
-                </select>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button
-          className="bg-[#cc98dc] w-36 h-10 rounded text-white lg:ml-20 md:ml-8 sm:ml-20 xs:ml-20 bg-purple-100 mt-2"
-          onClick={convert}
-          id="convert-btn"
-        >
-          Exchange
-        </button>
-      </div>
+                <td>
+                  <input
+                    required
+                    className="h-8 lg:w-40 md:w-20 rounded mb-2 bg-purple-100 text-center"
+                    value={inputValue}
+                    type="number"
+                    minLength="1"
+                    name="currency-amount-1"
+                    placeholder="Enter value"
+                    onChange={(e) => {
+                      setinputValue(e.target.value);
+                    }}
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td className="text-red-600">To</td>
+
+                <td>
+                  <select
+                    className="w-20  p-1 rounded  bg-purple-100"
+                    onChange={(e) => setvalue2(e.target.value)}
+                  >
+                    {coin &&
+                      Object.values(coin).map((d, k) => (
+                        <option key={k} value={d.value}>
+                          {d.name}
+                        </option>
+                      ))}
+                  </select>
+                </td>
+                <td>
+                  <p className=" bg-purple-100 text-center">{inputValueTwo}</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <button
+            type="submit"
+            className="bg-[#cc98dc] w-36 h-10 rounded text-white lg:ml-20 md:ml-8 sm:ml-20 xs:ml-20 bg-purple-100 mt-2"
+            onClick={convert}
+            id="convert-btn"
+          >
+            Exchange
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
